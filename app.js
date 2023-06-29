92e3322fbdb2ebae0c6ed529ff498ef75d698a13
samaysonje <95526810+samaysonje@users.noreply.github.com>
2023-06-29 14:07:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/blog",
+  // callbackURL: "http://localhost:3000/auth/google/blog",
+  callbackURL: "https://blog-project-37cb.onrender.com/auth/google/blog",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
